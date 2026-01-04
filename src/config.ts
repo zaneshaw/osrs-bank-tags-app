959e10d3abfb8395892ddaa8217b381d4ed98365
Bennett Fife <bennett.fife@gmail.com>
2026-01-03 16:54:25 -0800

    remove service role key for now

--- src/config.ts
+++ src/config.ts
@@ -7,11 +7,5 @@ if (!import.meta.env.VITE_SUPABASE_URL) {
   throw new Error('VITE_SUPABASE_URL is required');
 }
 
-if (!import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY) {
-  alert('VITE_SUPABASE_SERVICE_ROLE_KEY is required');
-  throw new Error('VITE_SUPABASE_SERVICE_ROLE_KEY is required');
-}
-
 export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-export const SUPABASE_SERVICE_ROLE_KEY = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
